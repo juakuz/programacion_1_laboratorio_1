c49f3204551b7a279d8ff78f1d6c15c4b763ce7c
juakuz <shoaking@gmail.com>
2018-03-26 11:24:58 -0300

    Add files via upload

---
+++ funcion/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int sumar(int numero1, int numero2);
+int leernumero (void);
+void mostrar (void);
+void pedirnumero (void);
+int pediryleernumero(void);
+
+float calcular(int primernumero; char op, int segundonumero);
+
+
+int main()
+{
+    int numero1;
+    float numero2;
+    float suma;
+    char operacion;
+    float resultado;
+
+    numero1= pediryleernumero();
+    printf("\ningrese operacion: ");
+    operacion=getche();
+    numero2=pediryleernumero();
+
+}
